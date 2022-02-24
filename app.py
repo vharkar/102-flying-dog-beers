74a0f9f4c771fd8d6f3c778a5d1bc64d072118e8
vharikar <vibha_harikar@intuit.com>
2022-02-24 00:31:37 +0000

    Changed beer color and name

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
+beers=['Witches Green', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[40, 60, 85, 85]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
+color1='darkgreen'
 color2='orange'
 mytitle='Beer Comparison'
 tabtitle='beer!'
